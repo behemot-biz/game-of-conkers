cd280bf07cf716ebf658372945cd3fc85671c9c6
Petra <petra@behemot.biz>
2024-05-10 11:45:36 +0200

    Add functions (empty) to outline the game logic and data needed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,56 @@
 
+/**
+ * create conker objects with random strength and durability
+ * if enough time add special attribute like stun, string snap etc.
+ */
 function createConker() {
+    
+}
+
+/**
+ * assign conkers to players (player and computer)
+ */
+function prepareGame () {
+    
+}
+/**
+ * print the conker stats through out the game
+ * 
+ */
+function displayConkerStats() {
+    //
 
 }
-function prepareGame () {
-    //assign conkers to players
+
+/**
+ * game loop
+ * keep track of th game turns computer or player's turn to hit
+ * Start new game on request when round has ended
+ * call quitGame on request
+ */
+function playGame() {
+
+    
+}
+
+/**
+ * Player and computer takes turns to hit conker untile one conker breakes
+ * calculate damage hit (strength)/durabillity until one player conker breaks i.e. durability = 0 
+ */
+function playTurn() {
+  
+}
+
+/**
+ * keep track of total score, first to take 10 wins wins the full game 
+ */
+function playerScore() {
+    //
+}
+
+/**
+ * Quit game
+ */
+function gameQuit() {
+    //Display thanks for a food game and welcome back.
 }
